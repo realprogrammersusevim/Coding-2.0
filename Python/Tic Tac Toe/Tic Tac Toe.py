4b757541c4664812913d3a228b9611be4cd92dfe
sarumanfartcatcher <93488695+sarumanfartcatcher@users.noreply.github.com>
2022-04-12 16:04:16 -0500

    Transferred global variables to seperate file.

--- Python/Tic Tac Toe/Tic Tac Toe.py
+++ Python/Tic Tac Toe/Tic Tac Toe.py
@@ -1,92 +1,66 @@
-one_one = "x"
-one_two = " "
-one_three = " "
-two_one = " "
-two_two = " "
-two_three = " "
-three_one = " "
-three_two = " "
-three_three = " "
+import config
 
-player_move = ''
 # This function will be called at the end of every move to display the current status of the board.
 
 
 def show_board():
-    global one_one
-    global two_one
-    global three_one
-    global one_two
-    global two_two
-    global two_three
-    global one_three
-    global two_three
-    global three_three
-
     print('  1   2   3')
-    print('1 ' + one_one + ' | ' + two_one + ' | ' + three_one)
+    print('1 ' + config.one_one + ' | ' +
+          config.two_one + ' | ' + config.three_one)
     print('  --+---+--')
-    print('2 ' + one_two + ' | ' + two_two + ' | ' + two_three)
+    print('2 ' + config.one_two + ' | ' +
+          config.two_two + ' | ' + config.three_two)
     print('  --+---+--')
-    print('3 ' + one_three + ' | ' + two_three + ' | ' + three_three)
+    print('3 ' + config.one_three + ' | ' +
+          config.two_three + ' | ' + config.three_three)
 
 # The point of this variable is to get where the player would like to go, and then change the correct variable.
 
 
 def ask_for_move():
-    global player_move_x
-    player_move_x = input("Where would you like to go on the x axis? ")
+    config.player_move_x = input("Where would you like to go on the x axis? ")
 
     # Checks if the player gave the correct input
-    # while player_move_x != '1' or '2' or '3':
-    # player_move_x = input("Where would you like to go on the x axis? ")
+    # while config.player_move_x != '1' or '2' or '3':
+    # config.player_move_x = input("Where would you like to go on the x axis? ")
 
-    global player_move_y
-    player_move_y = input("Where would you like to go on the y axis? ")
-    # while player_move_y != '1' or '2' or '3':
-    # player_move_y = input("Where would you like to go on the y axis? ")
+    config.player_move_y = input("Where would you like to go on the y axis? ")
+    # while config.player_move_y != '1' or '2' or '3':
+    # config.player_move_y = input("Where would you like to go on the y axis? ")
 
     # TODO Check if the space is already occupied before declaring it a valid move.
-    global player_move
-    player_move = (player_move_x + ', ' + player_move_y)
+    config.player_move = (config.player_move_x + ', ' + config.player_move_y)
 
-    if player_move == '1, 1':
-        global one_one
-        one_one = 'o'
-    elif player_move == ' 2, 1':
-        global two_one
-        two_one = 'o'
-    elif player_move == '3, 1':
-        global three_one
-        three_one = 'o'
-    elif player_move == '1, 2':
-        global one_two
-        one_two = 'o'
-    elif player_move == '2, 2':
-        global two_two
-        two_two = 'o'
-    elif player_move == '3, 2':
-        global three_two
-        three_two = 'o'
-    elif player_move == '1, 3':
-        global one_three
-        one_three = 'o'
-    elif player_move == '2, 3':
-        global two_three
-        two_three = 'o'
-    elif player_move == '3, 3':
-        global three_three
-        three_three = 'o'
+    if config.player_move == '1, 1':
+        config.one_one = 'o'
+    elif config.player_move == ' 2, 1':
+        config.two_one = 'o'
+    elif config.player_move == '3, 1':
+        config.three_one = 'o'
+    elif config.player_move == '1, 2':
+        config.one_two = 'o'
+    elif config.player_move == '2, 2':
+        config.two_two = 'o'
+    elif config.player_move == '3, 2':
+        config.three_two = 'o'
+    elif config.player_move == '1, 3':
+        config.one_three = 'o'
+    elif config.player_move == '2, 3':
+        config.two_three = 'o'
+    elif config.player_move == '3, 3':
+        config.three_three = 'o'
 
     show_board()
 
 
 def computer_wins():
+    show_board()
     print('The computer wins. Humanity is doomed.')
     # Eventually I'll start storing the win-tie record.
 
 
 def tie():
+    show_board()
     print('You tied up the game. Good job not-loser.')
 
 
@@ -96,279 +70,303 @@ show_board()
 
 
 ask_for_move()
-if player_move == '2, 1':
-    two_two = 'x'
+if config.player_move == '2, 1':
+    config.two_two = 'x'
     print("2, 2")
     ask_for_move()
+    show_board()
 
-    if player_move == '3, 1' or '1, 2' or '3, 2' or '1, 3' or '2, 3':
-        three_three = 'x'
+    if config.player_move == '3, 1' or '1, 2' or '3, 2' or '1, 3' or '2, 3':
+        config.three_three = 'x'
         print('3, 3')
         computer_wins()
-    elif player_move == '3, 3':
-        one_three = 'x'
+    elif config.player_move == '3, 3':
+        config.one_three = 'x'
         print('1, 3')
         ask_for_move()
+        show_board()
 
-        if player_move == '3, 1' or '3, 2' or '2, 3':
-            one_two = 'x'
+        if config.player_move == '3, 1' or '3, 2' or '2, 3':
+            config.one_two = 'x'
             print('1, 2')
             computer_wins()
-        elif player_move == '1, 2':
-            three_one = 'x'
+        elif config.player_move == '1, 2':
+            config.three_one = 'x'
             print('3, 1')
             computer_wins()
-elif player_move == '3, 1':
-    one_three = 'x'
+elif config.player_move == '3, 1':
+    config.one_three = 'x'
     print("1, 3")
     ask_for_move()
+    show_board()
 
-    if player_move == '2, 1' or '2, 2' or '3, 2' or '2, 3' or '3, 3':
-        one_two = 'x'
+    if config.player_move == '2, 1' or '2, 2' or '3, 2' or '2, 3' or '3, 3':
+        config.one_two = 'x'
         print('1, 2')
         computer_wins()
 
-    elif player_move == '1, 2':
-        three_three = 'x'
+    elif config.player_move == '1, 2':
+        config.three_three = 'x'
         print('3, 3')
         ask_for_move()
+        show_board()
 
-        if player_move == '2, 1' or '2, 2' or '3, 2':
-            two_three = 'x'
+        if config.player_move == '2, 1' or '2, 2' or '3, 2':
+            config.two_three = 'x'
             print('2, 3')
             computer_wins()
 
-        elif player_move == '2, 3':
-            two_two = 'x'
+        elif config.player_move == '2, 3':
+            config.two_two = 'x'
             print('2, 2')
             computer_wins()
-elif player_move == '1, 2':
-    two_two = 'x'
+elif config.player_move == '1, 2':
+    config.two_two = 'x'
     print("2, 2")
     ask_for_move()
+    show_board()
 
-    if player_move == '2, 1' or '3, 1' or '3, 2' or '1, 3' or '2, 3':
-        three_three = 'x'
+    if config.player_move == '2, 1' or '3, 1' or '3, 2' or '1, 3' or '2, 3':
+        config.three_three = 'x'
         print('3, 3')
         computer_wins()
 
-    elif player_move == '3, 3':
-        three_one = 'x'
+    elif config.player_move == '3, 3':
+        config.three_one = 'x'
         print('3, 1')
         ask_for_move()
+        show_board()
 
-        if player_move == '3, 2' or '1, 3' or '2, 3':
-            two_one = 'x'
+        if config.player_move == '3, 2' or '1, 3' or '2, 3':
+            config.two_one = 'x'
             print('2, 1')
             computer_wins()
 
-        elif player_move == '2, 1':
-            one_three = 'x'
+        elif config.player_move == '2, 1':
+            config.one_three = 'x'
             print('1, 3')
             computer_wins()
-elif player_move == '2 ,2':
-    three_three = 'x'
+elif config.player_move == '2 ,2':
+    config.three_three = 'x'
     print("3, 3")
 
-    if player_move == '2, 1':
-        two_three = 'x'
+    if config.player_move == '2, 1':
+        config.two_three = 'x'
         print('2, 3')
         ask_for_move()
+        show_board()
 
-        if player_move == '3, 1' or '1, 2' or '3, 2':
-            two_three = 'x'
+        if config.player_move == '3, 1' or '1, 2' or '3, 2':
+            config.two_three = 'x'
             print('2, 3')
             computer_wins()
-        elif player_move == '1, 3':
-            three_one = 'x'
+        elif config.player_move == '1, 3':
+            config.three_one = 'x'
             print('3, 1')
             ask_for_move()
+            show_board()
 
-            if player_move == '1, 2':
-                three_two = 'x'
+            if config.player_move == '1, 2':
+                config.three_two = 'x'
                 print('1, 2')
                 computer_wins()
-            elif player_move == '3, 2':
-                one_two = 'x'
+            elif config.player_move == '3, 2':
+                config.one_two = 'x'
                 print('1, 2')
                 tie()
-    elif player_move == '3, 1':
-        one_three = 'x'
+    elif config.player_move == '3, 1':
+        config.one_three = 'x'
         print('1, 3')
         ask_for_move()
+        show_board()
 
-        if player_move == '2, 1' or '2, 3':
-            one_two = 'x'
+        if config.player_move == '2, 1' or '2, 3':
+            config.one_two = 'x'
             print('1, 2')
             computer_wins()
-        elif player_move == '1, 2' or '3, 2':
-            two_three = 'x'
+        elif config.player_move == '1, 2' or '3, 2':
+            config.two_three = 'x'
             print('2, 3')
             computer_wins()
-    elif player_move == '1, 2':
-        three_two = 'x'
+    elif config.player_move == '1, 2':
+        config.three_two = 'x'
         print('3, 2')
         ask_for_move()
+        show_board()
 
-        if player_move == '2, 1' or '1, 3' or '2, 3':
-            three_two = 'x'
+        if config.player_move == '2, 1' or '1, 3' or '2, 3':
+            config.three_two = 'x'
             print('3, 2')
             computer_wins()
-        elif player_move == '3, 1':
-            one_three
+        elif config.player_move == '3, 1':
+            config.one_three
             print('1, 3')
             ask_for_move()
+            show_board()
 
-            if player_move == '2, 1':
-                two_three = 'x'
+            if config.player_move == '2, 1':
+                config.two_three = 'x'
                 print('2, 3')
                 computer_wins()
-            elif player_move == '2, 3':
-                two_one = 'x'
+            elif config.player_move == '2, 3':
+                config.one_one = 'x'
                 print('2, 1')
                 tie()
-    elif player_move == '3, 2':
-        one_two = 'x'
+    elif config.player_move == '3, 2':
+        config.one_two = 'x'
         print('1, 2')
         ask_for_move()
+        show_board()
 
-        if player_move == '2, 1' or '3, 1' or '2, 3':
-            one_three = 'x'
-            print(1, 3)
+        if config.player_move == '2, 1' or '3, 1' or '2, 3':
+            config.one_three = 'x'
+            print('1, 3')
             computer_wins()
-        elif player_move == '1, 3':
-            three_one = 'x'
+        elif config.player_move == '1, 3':
+            config.three_one = 'x'
             print('3, 1')
             ask_for_move()
+            show_board()
 
-            if player_move == '2, 1':
-                two_three = 'x'
+            if config.player_move == '2, 1':
+                config.two_three = 'x'
                 print('2, 3')
                 computer_wins()
-            elif player_move == '2, 3':
-                two_one = 'x'
+            elif config.player_move == '2, 3':
+                config.two_one = 'x'
                 print('2, 1')
                 computer_wins()
-    elif player_move == '1, 3':
-        three_one = 'x'
+    elif config.player_move == '1, 3':
+        config.three_one = 'x'
         print('3, 1')
         ask_for_move()
+        show_board()
 
-        if player_move == '2, 1' or '2, 3':
-            three_two = 'x'
+        if config.player_move == '2, 1' or '2, 3':
+            config.three_two = 'x'
             print('3, 2')
             computer_wins()
-        elif player_move == '1, 2' or '2, 3':
-            two_one = 'x'
+        elif config.player_move == '1, 2' or '2, 3':
+            config.two_one = 'x'
             print('2, 1')
             computer_wins()
-    elif player_move == '2, 3':
-        two_one = 'x'
+    elif config.player_move == '2, 3':
+        config.one_one = 'x'
         print('2, 1')
         ask_for_move()
+        show_board()
 
-        if player_move == '1, 2' or '3, 2' or '1, 3':
-            three_one = 'x'
+        if config.player_move == '1, 2' or '3, 2' or '1, 3':
+            config.three_one = 'x'
             print('3, 1')
             computer_wins()
-        elif player_move == '3, 1':
-            one_three = 'x'
+        elif config.player_move == '3, 1':
+            config.one_three = 'x'
             print('1, 3')
             ask_for_move()
+            show_board()
 
-            if player_move == '1, 2':
-                three_two = 'x'
+            if config.player_move == '1, 2':
+                config.three_two = 'x'
                 print('3, 2')
                 computer_wins()
-            elif player_move == '3, 2':
-                one_two = 'x'
+            elif config.player_move == '3, 2':
+                config.one_two = 'x'
                 print('1, 2')
                 tie()
-elif player_move == '3, 2':
-    two_two = 'x'
+elif config.player_move == '3, 2':
+    config.two_two = 'x'
     print('2, 2')
     ask_for_move()
+    show_board()
 
-    if player_move == '2, 1' or '3, 1' or '1, 2' or '1, 3' or '2, 3':
-        three_three = 'x'
+    if config.player_move == '2, 1' or '3, 1' or '1, 2' or '1, 3' or '2, 3':
+        config.three_three = 'x'
         print('3, 3')
         computer_wins()
-    elif player_move == '3, 3':
-        three_one = 'x'
+    elif config.player_move == '3, 3':
+        config.three_one = 'x'
         print('3, 1')
         ask_for_move()
+        show_board()
 
-        if player_move == '1, 2' or '1, 3' or '2, 3':
-            two_one = 'x'
+        if config.player_move == '1, 2' or '1, 3' or '2, 3':
+            config.two_one = 'x'
             print('2, 1')
             computer_wins()
-        elif player_move == '2, 1':
-            one_three = 'x'
+        elif config.player_move == '2, 1':
+            config.one_three = 'x'
             print('1, 3')
             computer_wins()
-elif player_move == '1, 3':
-    three_one = 'x'
+elif config.player_move == '1, 3':
+    config.three_one = 'x'
     print('3, 1')
     ask_for_move()
+    show_board()
 
-    if player_move == '1, 2' or '2, 2' or '3, 2' or '2, 3' or '3, 3':
-        two_one = 'x'
+    if config.player_move == '1, 2' or '2, 2' or '3, 2' or '2, 3' or '3, 3':
+        config.two_one = 'x'
         print('2,1')
         computer_wins()
-    elif player_move == '2, 1':
-        three_three = 'x'
+    elif config.player_move == '2, 1':
+        config.three_three = 'x'
         print('3, 3')
         ask_for_move()
+        show_board()
 
-        if player_move == '1, 2' or '2, 2' or '2, 3':
-            three_two = 'x'
+        if config.player_move == '1, 2' or '2, 2' or '2, 3':
+            config.three_two = 'x'
             print('3, 2')
             computer_wins()
-        elif player_move == '3, 2':
-            two_two = 'x'
+        elif config.player_move == '3, 2':
+            config.two_two = 'x'
             print('2, 2')
             computer_wins()
-elif player_move == '2, 3':
-    two_two = 'x'
+elif config.player_move == '2, 3':
+    config.two_two = 'x'
     print('2, 2')
     ask_for_move()
+    show_board()
 
-    if player_move == '2, 1' or '3, 1' or '1, 2' or '3, 2' or '1, 3':
-        three_three = 'x'
+    if config.player_move == '2, 1' or '3, 1' or '1, 2' or '3, 2' or '1, 3':
+        config.three_three = 'x'
         print('3, 3')
         computer_wins()
-    elif player_move == '3, 3':
-        one_three = 'x'
+    elif config.player_move == '3, 3':
+        config.one_three = 'x'
         print('1, 3')
         ask_for_move()
+        show_board()
 
-        if player_move == '2, 1' or '3, 1':
-            one_two = 'x'
+        if config.player_move == '2, 1' or '3, 1':
+            config.one_two = 'x'
             print('1, 2')
             computer_wins()
-        elif player_move == '1, 2' or '3, 2':
-            three_two = 'x'
+        elif config.player_move == '1, 2' or '3, 2':
+            config.three_two = 'x'
             print('3, 2')
             computer_wins()
-elif player_move == '3, 3':
-    three_one = 'x'
+elif config.player_move == '3, 3':
+    config.three_one = 'x'
     print('3, 1')
     ask_for_move()
+    show_board()
 
-    if player_move == '1, 2' or '2, 2' or '3, 2' or '1, 3' or '2, 3':
-        two_one = 'x'
+    if config.player_move == '1, 2' or '2, 2' or '3, 2' or '1, 3' or '2, 3':
+        config.two_one = 'x'
         print('2, 1')
         computer_wins()
-    elif player_move == '2, 1':
-        one_three = 'x'
+    elif config.player_move == '2, 1':
+        config.one_three = 'x'
         print('1, 3')
         ask_for_move()
+        show_board()
 
-        if player_move == '2, 2' or '3, 2' or '2, 3':
-            one_two = 'x'
+        if config.player_move == '2, 2' or '3, 2' or '2, 3':
+            config.one_two = 'x'
             print('1, 2')
             computer_wins()
-        elif player_move == '1, 2':
-            two_two = 'x'
+        elif config.player_move == '1, 2':
+            config.two_two = 'x'
             print('2, 2')
             computer_wins()
---
+++ Python/Tic Tac Toe/config.py
@@ -0,0 +1,14 @@
+one_one = "x"
+one_two = " "
+one_three = " "
+two_one = " "
+two_two = " "
+two_three = " "
+three_one = " "
+three_two = " "
+three_three = " "
+
+player_move = ''
+
+player_move_x = ''
+player_move_y = ''
