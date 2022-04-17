14b636ece742949813444a4c97432d24b7065e8f
sarumanfartcatcher <93488695+sarumanfartcatcher@users.noreply.github.com>
2022-04-17 18:23:49 -0500

    Updated bash script. Works now.

--- Shell Scripts/getblockchaininfo.sh
+++ Shell Scripts/getblockchaininfo.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# This is a script to query my rasperry pi about the bitcoin blockchain info from the fullnode that it's running.
-# TODO Find out how to check if the rasperry pi is reachable before attempting to run commands.
-date=$(date +%Y%m%d)
-echo $date
+# This is a script to query my Rasperry Pi about the Bitcoin blockchain info from the fullnode that it's running.
+# TODO: Find out how to check if the rasperry pi is reachable before attempting to run commands.
 
-ssh rpi "bitcoin-cli getblockchaininfo"
-sleep 2
-echo "Queried blockchain info"
-echo $blockchaininfooutput
+ping -c1 192.168.0.43 > /dev/null
+if [ $? -eq 0 ]
+then
+    echo "What info would you like to get from the Raspberry Pi?"
+    read BITCOIN_USER_COMMAND
+    echo "About to send the ssh command to the RPi"
+    ssh rpi "bitcoin-cli $BITCOIN_USER_COMMAND"
+fi
+
+echo "Completed the script"
+
+exit 0
