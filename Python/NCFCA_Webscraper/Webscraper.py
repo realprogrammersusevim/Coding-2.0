3de20dfdc3c7ade91a7a5ed88dbccf27a368e2cd
realprogrammersusevim <jonathan@themilligans.com>
2022-06-21 11:06:22 -0500

    Update NCFCA Webscraper
    Updated the NCFCA webscraper to have a configuration file using yaml where the user can put in their credentials and the xpath for the “enter tournament” button. This should make configuration easier for the user so they won’t have to mess around in the source code.
    
    Signed-off-by: realprogrammersusevim <jonathan@themilligans.com>

--- Python/NCFCA_Webscraper/Webscraper.py
+++ Python/NCFCA_Webscraper/Webscraper.py
@@ -10,17 +10,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import applescript
+import pickle
+import yaml
 
 print('How many minutes would you like the script to run for?')
 minutes = input()
-minutes = int(minutes)
+
+try:
+    minutes = int(minutes)
+except:
+    print('Please put in the number of minutes you would like this to run for')
+    minutes = int(input())
+
 repititions = minutes / 2
 repititions = round(repititions)
 
-# Starts a browser session I need to clean all of this code up. It's partly
-# from Stack Overflow and then heavily edited by Jonathan. Some of it is now
-# unnecessary. This is the problem with coding. It's awful code, but i t does
-# work so I don't want to touch it.
+# Starts a browser session I need to clean all of this code up.
 options = webdriver.ChromeOptions()
 options.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
 DRIVER_PATH = '/usr/local/bin/chromedriver'
@@ -30,17 +35,24 @@ driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 driver.get("https://portal.ncfca.org")
 driver.implicitly_wait(30)
 
+try:
+    with open('my_secret_creds.yaml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+except:
+    with open('credentials.yaml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+
 # Finds the text box where you enter your email by the html name, clicks it,
 # and then fills it out with my email.
 email_login = driver.find_element(By.ID, 'mat-input-9')
 email_login.click()
-email_login.send_keys("jonathanmilligan42@icloud.com")
+email_login.send_keys(data.get('username'))
 
 # Finds the text box where you enter your password by the html name, clicks it,
 # and then fills it out with my email.
 password_login = driver.find_element(By.ID, "mat-input-10")
 password_login.click()
-password_login.send_keys("clVy>VA:65")
+password_login.send_keys(data.get('password'))
 sleep(2)
 
 # Find the Log In button by its Xpath and then click it.
@@ -51,7 +63,8 @@ driver.implicitly_wait(30)
 
 # Finds the Enter Tournament button by its Xpath and then clicks on it.
 def click_tournament_enter():
-    enter_tournament = driver.find_element(By.XPATH, '''/html/body/app-root/mat-sidenav-container/mat-sidenav-content/div/app-dashboard/app-dashboard-family/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[5]/div/button[2]''')
+    enter_tournament = driver.find_element(
+        By.XPATH, data.get('enter_tournament_button_xpath'))
     enter_tournament.click()
 
 
@@ -61,21 +74,20 @@ except:
     sleep(10)
     click_tournament_enter()
 
-f = open('var_storage.txt', 'x')
-f = open('var_storage.txt', 'w')
-f.write('0')
+with open('var_storage.p', 'xw') as f:
+    f.pickle('0')
 
 for i in range(repititions):
     html = driver.page_source   # Renders the JS and stores it as static HTML
-    soup = BeautifulSoup(html, "html.parser")   # Sets the rendered JS to a Beautiful Soup object
-
+    # Sets the rendered JS to a Beautiful Soup object
+    soup = BeautifulSoup(html, "html.parser")
 
     # No more website interaction is needed, so we can safely quit the driver
     driver.quit()
 
     # Finds every piece of content that has the class name of a posted listing
     activated_postings = soup.find_all(
-        class_ = 'schedule-list-item active ng-star-inserted')
+        class_='schedule-list-item active ng-star-inserted')
 
     # Checks if there are any more listings have been posted
     count = 0
@@ -84,19 +96,17 @@ for i in range(repititions):
 
     # Currently it's just using a text file to store how many listings were
     # posted last time. I may need a better solution like pickle.
-    f = open('var_storage.txt', 'r')
-    stored_number = f.read()
-    f.close()
+    with open('var_storage.p', 'r') as f:
+        stored_number = f.unpickle()
 
     # If there are more listings posted now then there were before than it
     # executes an AppleScript which sends a notification and then updates the
     # number.
-    if stored_number <= count:
+    if stored_number < count:
         send_notification = applescript.run('''display notification "New listings have been
         posted." with title "New Listings"''')
-        file = open('var_storage.txt', 'w')
-        file.write(stored_number)
-        file.close()
+        with open('var_storage.p', 'w') as f:
+            f.pickle(count)
 
     sleep(120)
     driver.refresh()
