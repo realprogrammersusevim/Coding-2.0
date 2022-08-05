d42f45b2ab12c211426440624bb2c0e69f78fbd9
realprogrammersusevim <logouts.deco-0e@icloud.com>
2022-08-05 13:24:17 -0500

    Fix Tic Tac Toe Bugs
    Made improvements to make the Tic Tac Toe program work more consistently
    and fixed some stupid mistakes I had made.
    1. I forgot that Numpy arrays index from zero
    2. It now correctly iterates over the matrix when displaying the board
    
    Signed-off-by: realprogrammersusevim <logouts.deco-0e@icloud.com>

--- Python/Tic_Tac_Toe/tic_tac_toe.py
+++ Python/Tic_Tac_Toe/tic_tac_toe.py
@@ -12,17 +12,18 @@ def show_board():
     player_board = np.rot90(variables.board, variables.rotation)
     count = 0
 
-    for i in player_board:
-        match variables.board.all():
-            case 2:
-                print('o', end=' ')
-            case 1:
-                print('x', end=' ')
-            case 0:
-                print('-', end=' ')
-        count += 1
-        if count == 3 or count == 6 or count == 9:
-            print('\n')
+    for row in player_board:
+        for cell in row:
+            match cell:
+                case 2:
+                    print("o", end=" ")
+                case 1:
+                    print("x", end=" ")
+                case 0:
+                    print("-", end=" ")
+            count += 1
+            if count == 3 or count == 6 or count == 9:
+                print("\n")
 
 
 # 0 means the space is unoccupied, 1 that it is an X and 2 that it's an O
@@ -31,30 +32,26 @@ def ask_for_move():
     Asks for where the player would like to go and changes the correct variable
     """
 
-    variables.player_move_x = input(
-        "Where would you like to go on the x axis? ")
+    variables.player_move_x = input("Where would you like to go on the x axis? ")
 
-    variables.player_move_y = input(
-        "Where would you like to go on the y axis? ")
+    variables.player_move_y = input("Where would you like to go on the y axis? ")
 
-    variables.player_move = (
-        f'{variables.player_move_x}, {variables.player_move_y}')
+    variables.player_move = f"{variables.player_move_x}, {variables.player_move_y}"
 
     # This is the player board that is simply the real board that is rotated to
     # look different to the player
     player_board = np.rot90(variables.board, variables.rotation)
 
     # Checks that the player's move is valid before changing the board
-    if player_board[int(variables.player_move_y), int(variables.player_move_x)] != 0:
-        print('That space is already occupied. Try again.')
+    if player_board[int(variables.player_move_y) - 1, int(variables.player_move_x) - 1] != 0:
+        print("That space is already occupied. Try again.")
         ask_for_move()
 
     print(variables.player_move)
 
     # Remember that I use the Cartesian coordinate system (X then Y) while the
     # matrix uses Y first and X second
-    player_board[int(variables.player_move_y),
-                 int(variables.player_move_x)] = 2
+    player_board[int(variables.player_move_y) - 1, int(variables.player_move_x) - 1] = 2
 
     sleep(0.2)
     show_board()
@@ -68,22 +65,28 @@ def ask_for_move():
 # TODO: Change the board to a matrix for better computer manipulation. Then I
 # just need a good function to translate the matrix into a GUI and back again.
 
+
 def computer_move(x_axis, y_axis):
     "Edits the np matrix board"
-    variables.board[int(y_axis), int(x_axis)] = 1
-    print(f'{x_axis}, {y_axis}')
+    variables.board[int(y_axis) - 1, int(x_axis) - 1] = 1
+    print(f"{x_axis}, {y_axis}")
     show_board()
 
 
 def computer_wins():
+    """
+    Called when the computer wins.
+    """
     show_board()
-    print('The computer wins. Humanity is doomed.')
+    print("The computer wins. Humanity is doomed.")
     # Eventually I'll start storing the win-tie record with Pickle.
 
 
 def tie():
+    "This function will be called when the board is full and no one has won."
     show_board()
-    print('You tied up the game. Good job not-loser.')
+    show_board()
+    print("You tied up the game. Good job not-loser.")
 
 
 # First int is x value second is y
@@ -93,240 +96,240 @@ show_board()
 
 ask_for_move()
 match variables.player_move:
-    case '2, 1':
+    case "2, 1":
         computer_move(2, 1)
         ask_for_move()
 
         match variables.player_move:
-            case '3, 1' | '1, 2' | '3, 2' | '1, 3' | '2, 3':
+            case "3, 1" | "1, 2" | "3, 2" | "1, 3" | "2, 3":
                 computer_move(3, 3)
                 computer_wins()
-            case '3, 3':
+            case "3, 3":
                 computer_move(1, 3)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '3, 1' | '3, 2' | '2, 3':
+                    case "3, 1" | "3, 2" | "2, 3":
                         computer_move(1, 2)
                         computer_wins()
-                    case '1, 2':
+                    case "1, 2":
                         computer_move(3, 1)
                         computer_wins()
-    case '3, 1':
+    case "3, 1":
         computer_move(1, 3)
         ask_for_move()
 
         match variables.player_move:
-            case '2, 1' | '2, 2' | '3, 2' | '2, 3' | '3, 3':
+            case "2, 1" | "2, 2" | "3, 2" | "2, 3" | "3, 3":
                 computer_move(1, 2)
                 computer_wins()
-            case '1, 2':
+            case "1, 2":
                 computer_move(3, 3)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '2, 1' | '2, 2' | '3, 2':
+                    case "2, 1" | "2, 2" | "3, 2":
                         computer_move(2, 3)
                         computer_wins()
-                    case '2, 3':
+                    case "2, 3":
                         computer_move(2, 2)
                         computer_wins()
-    case '1, 2':
+    case "1, 2":
         computer_move(2, 2)
         ask_for_move()
 
         match variables.player_move:
-            case '2, 1' | '3, 1' | '3, 2' | '1, 3' | '2, 3':
+            case "2, 1" | "3, 1" | "3, 2" | "1, 3" | "2, 3":
                 computer_move(3, 3)
                 computer_wins()
 
-            case '3, 3':
+            case "3, 3":
                 computer_move(3, 1)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '3, 2' | '1, 3' | '2, 3':
+                    case "3, 2" | "1, 3" | "2, 3":
                         computer_move(2, 1)
                         computer_wins()
-                    case '2, 1':
+                    case "2, 1":
                         computer_move(1, 3)
                         computer_wins()
-    case '2, 2':
+    case "2, 2":
         computer_move(3, 3)
         ask_for_move()
 
         match variables.player_move:
-            case '2, 1':
+            case "2, 1":
                 computer_move(2, 3)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '3, 1' | '1, 2' | '3, 2':
+                    case "3, 1" | "1, 2" | "3, 2":
                         computer_move(2, 3)
                         computer_wins()
-                    case '1, 3':
+                    case "1, 3":
                         computer_move(3, 1)
                         ask_for_move()
 
                         match variables.player_move:
-                            case '1, 2':
+                            case "1, 2":
                                 computer_move(1, 2)
                                 computer_wins()
-                            case '3, 2':
+                            case "3, 2":
                                 computer_move(1, 2)
                                 tie()
-            case '3, 1':
+            case "3, 1":
                 computer_move(1, 3)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '2, 3':
+                    case "2, 3":
                         computer_move(1, 2)
                         computer_wins()
-                    case '1, 2' | '3, 2':
+                    case "1, 2" | "3, 2":
                         computer_move(2, 3)
                         computer_wins()
-            case '1, 2':
+            case "1, 2":
                 computer_move(3, 2)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '2, 1' | '1, 3' | '2, 3':
+                    case "2, 1" | "1, 3" | "2, 3":
                         computer_move(3, 1)
                         computer_wins()
-                    case '3, 1':
+                    case "3, 1":
                         computer_move(1, 3)
                         ask_for_move()
 
                         match variables.player_move:
-                            case '2, 1':
+                            case "2, 1":
                                 computer_move(2, 3)
                                 computer_wins()
-                            case '2, 3':
+                            case "2, 3":
                                 computer_move(2, 1)
                                 tie()
-            case '3, 2':
+            case "3, 2":
                 computer_move(1, 2)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '2, 1' | '3, 1' | '2, 3':
+                    case "2, 1" | "3, 1" | "2, 3":
                         computer_move(1, 3)
                         computer_wins()
-                    case '1, 3':
+                    case "1, 3":
                         computer_move(3, 1)
                         ask_for_move()
 
                         match variables.player_move:
-                            case '2, 1':
+                            case "2, 1":
                                 computer_move(2, 3)
                                 computer_wins()
-                            case '2, 3':
+                            case "2, 3":
                                 computer_move(2, 1)
                                 computer_wins()
-            case '1, 3':
+            case "1, 3":
                 computer_move(3, 1)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '2, 1' | '2, 3':
+                    case "2, 1" | "2, 3":
                         computer_move(3, 2)
                         computer_wins()
-                    case '1, 2' | '2, 3' | '3, 2':
+                    case "1, 2" | "2, 3" | "3, 2":
                         computer_move(2, 1)
                         computer_wins()
-            case '2, 3':
+            case "2, 3":
                 computer_move(2, 1)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '1, 2' | '3, 2' | '1, 3':
+                    case "1, 2" | "3, 2" | "1, 3":
                         computer_move(3, 1)
                         computer_wins()
-                    case '3, 1':
+                    case "3, 1":
                         computer_move(1, 3)
                         ask_for_move()
 
                         match variables.player_move:
-                            case '1, 2':
+                            case "1, 2":
                                 computer_move(3, 2)
                                 computer_wins()
-                            case '3, 2':
+                            case "3, 2":
                                 computer_move(1, 2)
                                 tie()
-    case '3,2':
+    case "3,2":
         computer_move(2, 2)
         ask_for_move()
 
         match variables.player_move:
-            case '2, 1' | '3, 1' | '1, 2' | '1, 3' | '2, 3':
+            case "2, 1" | "3, 1" | "1, 2" | "1, 3" | "2, 3":
                 computer_move(3, 3)
                 computer_wins()
-            case '3, 3':
+            case "3, 3":
                 computer_move(3, 1)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '1, 2' | '1, 3' | '2, 3':
+                    case "1, 2" | "1, 3" | "2, 3":
                         computer_move(2, 1)
                         computer_wins()
-                    case '2, 1':
+                    case "2, 1":
                         computer_move(1, 3)
                         computer_wins()
-    case '1, 3':
+    case "1, 3":
         computer_move(3, 1)
         ask_for_move()
 
         match variables.player_move:
-            case '1, 2' | '2, 2' | '3, 2' | '2, 3' | '3, 3':
+            case "1, 2" | "2, 2" | "3, 2" | "2, 3" | "3, 3":
                 computer_move(2, 1)
                 computer_wins()
-            case '2, 1':
+            case "2, 1":
                 computer_move(3, 3)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '1, 2' | '2, 2' | '2, 3':
+                    case "1, 2" | "2, 2" | "2, 3":
                         computer_move(3, 2)
                         computer_wins()
-                    case '3, 2':
+                    case "3, 2":
                         computer_move(2, 2)
                         computer_wins()
-    case '2, 3':
+    case "2, 3":
         computer_move(2, 2)
         ask_for_move()
 
         match variables.player_move:
-            case '2, 1' | '3, 1' | '1, 2' '3, 2' | '1, 3':
+            case "2, 1" | "3, 1" | "1, 2" "3, 2" | "1, 3":
                 computer_move(3, 3)
                 computer_wins()
-            case '3, 3':
+            case "3, 3":
                 computer_move(1, 3)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '2, 1' | '3, 1':
+                    case "2, 1" | "3, 1":
                         computer_move(1, 2)
                         computer_wins()
-                    case '1, 2' | '3, 2':
+                    case "1, 2" | "3, 2":
                         computer_move(3, 2)
                         computer_wins()
-    case '3, 3':
+    case "3, 3":
         computer_move(3, 1)
         ask_for_move()
 
         match variables.player_move:
-            case '1, 2' | '2, 2' | '3, 2' | '1, 3' | '2, 3':
+            case "1, 2" | "2, 2" | "3, 2" | "1, 3" | "2, 3":
                 computer_move(2, 1)
                 computer_wins()
-            case '2, 1':
+            case "2, 1":
                 computer_move(1, 3)
                 ask_for_move()
 
                 match variables.player_move:
-                    case '2, 2' | '3, 2' | '2, 3':
+                    case "2, 2" | "3, 2" | "2, 3":
                         computer_move(1, 2)
                         computer_wins()
-                    case '1, 2':
+                    case "1, 2":
                         computer_move(2, 2)
                         computer_wins()
