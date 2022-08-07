bb842981dc103cc3e3a2bcccd7842438d4127769
realprogrammersusevim <logouts.deco-0e@icloud.com>
2022-08-07 15:21:40 -0500

    Update Axis and Allies Battle Calc
    Update axis_and_allies_calc.py to consolidate the code more instead of
    having it spread out over many different functions. The units are also
    now loaded from a YAML configuration file to make quick changes much
    easier for the user. The output is also now shown as a win rate
    percentage rather than as the raw wins and losses.
    
    The python_throw_calculator was also renamed to the throw_calculator.

--- Python/Miscellaneous Scripts/Need Work/axis_and_allies_calc.py
+++ Python/Miscellaneous Scripts/Need Work/axis_and_allies_calc.py
@@ -1,26 +1,26 @@
 #!python3
 import random
+import yaml
 
-attack_unit = {
-    "infantry": int(input("Enter the number of attacking infantry: ")),
-    "artillery": int(input("Enter the number of attacking artillery: ")),
-    "tanks": int(input("Enter the number of attacking tanks: ")),
-    "fighters": int(input("Enter the number of attacking fighters: ")),
-    "bombers": int(input("Enter the number of attacking bombers: ")),
-}
-
-defend_unit = {
-    "infantry": int(input("Enter the number of defending infantry: ")),
-    "artillery": int(input("Enter the number of defending artillery: ")),
-    "tanks": int(input("Enter the number of defending tanks: ")),
-    "fighters": int(input("Enter the number of defending fighters: ")),
-    "bombers": int(input("Enter the number of defending bombers: ")),
-}
-
+attack_unit = {}
+defend_unit = {}
 promoted_infantry = 0
-infantry = attack_unit["infantry"]
-while promoted_infantry != attack_unit["artillery"] and infantry != 0:
-    promoted_infantry += 1
+
+
+def get_units():
+    global attack_unit, defend_unit, promoted_infantry
+
+    with open("aaa_units.yaml", "r") as f:
+        units = yaml.full_load(f)
+
+    attack_unit = units["attack_unit"]
+    defend_unit = units["defend_unit"]
+    promoted_infantry = 0
+    infantry = attack_unit["infantry"]
+    while promoted_infantry < attack_unit["artillery"] and infantry > 0:
+        promoted_infantry += 1
+
+
 
 
 def roll_dice(num):
@@ -31,97 +31,13 @@ def roll_dice(num):
         return 0
 
 
-def compute_attack_hits():
-    attack_hits = 0
-
-    for i in range(attack_unit["infantry"]):
-        attack_hits += roll_dice(1)
-
-    for i in range(promoted_infantry):
-        attack_hits += roll_dice(2)
-
-    for i in range(attack_unit["artillery"]):
-        attack_hits += roll_dice(2)
-
-    for i in range(attack_unit["tanks"]):
-        attack_hits += roll_dice(3)
-
-    for i in range(attack_unit["fighters"]):
-        attack_hits += roll_dice(3)
-
-    for i in range(attack_unit["bombers"]):
-        attack_hits += roll_dice(4)
-
-    return attack_hits
-
-
-def compute_defend_hits():
-    defend_hits = 0
-
-    for i in range(defend_unit["infantry"]):
-        defend_hits += roll_dice(2)
-
-    for i in range(defend_unit["artillery"]):
-        defend_hits += roll_dice(2)
-
-    for i in range(defend_unit["tanks"]):
-        defend_hits += roll_dice(3)
-
-    for i in range(defend_unit["fighters"]):
-        defend_hits += roll_dice(4)
-
-    for i in range(defend_unit["bombers"]):
-        defend_hits += roll_dice(1)
-
-    return round(defend_hits)
-
-
-def take_losses():
-    attack_hits = compute_attack_hits()
-    defend_hits = compute_defend_hits()
-
-    while attack_hits != 0:
-        if attack_unit["infantry"] != 0:
-            attack_unit["infantry"] -= 1
-            attack_hits -= 1
-        elif attack_unit["artillery"] != 0:
-            attack_unit["artillery"] -= 1
-            attack_hits -= 1
-        elif attack_unit["tanks"] != 0:
-            attack_unit["tanks"] -= 1
-            attack_hits -= 1
-        elif attack_unit["fighters"] != 0:
-            attack_unit["fighters"] -= 1
-            attack_hits -= 1
-        elif attack_unit["bombers"] != 0:
-            attack_unit["bombers"] -= 1
-            attack_hits -= 1
-        else:
-            break
-
-    while defend_hits != 0:
-        if defend_unit["infantry"] != 0:
-            defend_unit["infantry"] -= 1
-            defend_hits -= 1
-        elif defend_unit["artillery"] != 0:
-            defend_unit["artillery"] -= 1
-            defend_hits -= 1
-        elif defend_unit["tanks"] != 0:
-            defend_unit["tanks"] -= 1
-            defend_hits -= 1
-        elif defend_unit["fighters"] != 0:
-            defend_unit["fighters"] -= 1
-            defend_hits -= 1
-        elif defend_unit["bombers"] != 0:
-            defend_unit["bombers"] -= 1
-            defend_hits -= 1
-        else:
-            break
-
-
-def run():
+attack_wins = 0
+defend_wins = 0
+for i in range(1000):
+    get_units()
     while (
         attack_unit["infantry"]
+        + promoted_infantry
         + attack_unit["artillery"]
         + attack_unit["tanks"]
         + attack_unit["fighters"]
@@ -134,32 +50,86 @@ def run():
         + defend_unit["bombers"]
         != 0
     ):
-        take_losses()
-        # print("\n")
-        # print("Attackers hit:", compute_attack_hits())
-        # print("Defenders hit:", compute_defend_hits())
-        # print("\n")
-        # print("Attackers remaining:")
-        # print("Infantry:", attack_unit["infantry"])
-        # print("Artillery:", attack_unit["artillery"])
-        # print("Tanks:", attack_unit["tanks"])
-        # print("Fighters:", attack_unit["fighters"])
-        # print("Bombers:", attack_unit["bombers"])
-        # print("\n")
-        # print("Defenders remaining:")
-        # print("Infantry:", defend_unit["infantry"])
-        # print("Artillery:", defend_unit["artillery"])
-        # print("Tanks:", defend_unit["tanks"])
-        # print("Fighters:", defend_unit["fighters"])
-        # print("Bombers:", defend_unit["bombers"])
+        attack_hits = 0
 
+        for i in range(attack_unit["infantry"]):
+            attack_hits += roll_dice(1)
 
-attack_wins = 0
-defend_wins = 0
-for i in range(1000):
-    run()
+        for i in range(promoted_infantry):
+            attack_hits += roll_dice(2)
+
+        for i in range(attack_unit["artillery"]):
+            attack_hits += roll_dice(2)
+
+        for i in range(attack_unit["tanks"]):
+            attack_hits += roll_dice(3)
+
+        for i in range(attack_unit["fighters"]):
+            attack_hits += roll_dice(3)
+
+        for i in range(attack_unit["bombers"]):
+            attack_hits += roll_dice(4)
+
+        defend_hits = 0
+
+        for i in range(defend_unit["infantry"]):
+            defend_hits += roll_dice(2)
+
+        for i in range(defend_unit["artillery"]):
+            defend_hits += roll_dice(2)
+
+        for i in range(defend_unit["tanks"]):
+            defend_hits += roll_dice(3)
+
+        for i in range(defend_unit["fighters"]):
+            defend_hits += roll_dice(4)
+
+        for i in range(defend_unit["bombers"]):
+            defend_hits += roll_dice(1)
+
+        while attack_hits != 0:
+            if attack_unit["infantry"] != 0:
+                attack_unit["infantry"] -= 1
+                attack_hits -= 1
+            elif promoted_infantry != 0:
+                promoted_infantry -= 1
+                attack_hits -= 1
+            elif attack_unit["artillery"] != 0:
+                attack_unit["artillery"] -= 1
+                attack_hits -= 1
+            elif attack_unit["tanks"] != 0:
+                attack_unit["tanks"] -= 1
+                attack_hits -= 1
+            elif attack_unit["fighters"] != 0:
+                attack_unit["fighters"] -= 1
+                attack_hits -= 1
+            elif attack_unit["bombers"] != 0:
+                attack_unit["bombers"] -= 1
+                attack_hits -= 1
+            else:
+                break
+
+        while defend_hits != 0:
+            if defend_unit["infantry"] != 0:
+                defend_unit["infantry"] -= 1
+                defend_hits -= 1
+            elif defend_unit["artillery"] != 0:
+                defend_unit["artillery"] -= 1
+                defend_hits -= 1
+            elif defend_unit["tanks"] != 0:
+                defend_unit["tanks"] -= 1
+                defend_hits -= 1
+            elif defend_unit["fighters"] != 0:
+                defend_unit["fighters"] -= 1
+                defend_hits -= 1
+            elif defend_unit["bombers"] != 0:
+                defend_unit["bombers"] -= 1
+                defend_hits -= 1
+            else:
+                break
     if (
         attack_unit["infantry"] == 0
+        and promoted_infantry == 0
         and attack_unit["artillery"] == 0
         and attack_unit["tanks"] == 0
         and attack_unit["fighters"] == 0
@@ -175,6 +145,7 @@ for i in range(1000):
     ):
         attack_wins += 1
 
-    print(attack_wins, defend_wins, end="\r")
-
-print("\n")
+print(f"""
+        Attacker winrate: {attack_wins/10}%
+        Defender winrate: {defend_wins/10}%
+        """)
