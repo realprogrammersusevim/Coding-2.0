cdcb15b61087579c2e65a9b651c9c6de35908ab3
realprogrammersusevim <logouts.deco-0e@icloud.com>
2022-09-01 13:23:54 -0500

    Improve renaming script
    I just learned about the "zfill" operator in Python. I had no idea that
    it existed. Anyway, it made having smarter leading zeroes far easier
    than my plan of iterating through the files multiple times.

--- Python/Miscellaneous Scripts/renamer.py
+++ Python/Miscellaneous Scripts/renamer.py
@@ -1,13 +1,12 @@
 import os
 
 dir = input("Enter the directory path: ")
-files = os.listdir(dir)
-num_files = len(files)
+fill = len(str(os.listdir(dir))) + 1
 counter = 0
 
-for file in files:
+# Rename files to numbers
+for file in os.listdir(dir):
     counter += 1
     file_extension = file.split(".")[-1]
-    # Rename the file to the counter with leading zeros
-    new_name = f"{counter:03d}.{file_extension}"
-    os.rename(f"{dir}/{file}", f"{dir}/{new_name}")
+    new_name = f"{str(counter).zfill(fill)}.{file_extension}"
+    os.rename(os.path.join(dir, file), os.path.join(dir, new_name))
