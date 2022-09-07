64e2b277c6c3edf5cc7ef6f27b65d40a227652a6
realprogrammersusevim <logouts.deco-0e@icloud.com>
2022-09-06 22:20:44 -0500

    Rewrite Python password generator
    Now that I know how to easily write command line tools in Python with
    click I decided to rewrite my password generator to not only be more
    random but to be a command line tool.

--- Python/Miscellaneous Scripts/passwords.py
+++ Python/Miscellaneous Scripts/passwords.py
@@ -1,15 +1,18 @@
-import secrets
+import string
+import click
 import random
 
-# macOS ships with a handy builtin word list. If you are on another OS you can substitute this file path for whatever comes with your OS.
-words = open("/usr/share/dict/words").read().splitlines()
 
-# This ugly line of code generates a random password with the form Word-Word-Word-WordNumberNumberNumberNumber
-password = f"{random.choice(words).lower()}-{random.choice(words).lower()}-{random.choice(words).lower()}-{random.choice(words).lower()}{secrets.randbelow(10)}{secrets.randbelow(10)}{secrets.randbelow(10)}{secrets.randbelow(10)}"
-print(f"Your password is: {password}")
+@click.command()
+@click.option("-l", "--length", default=16, help="Length of the password")
+@click.option("-n", "--number", default=1, help="Number of passwords to generate")
+def main(length, number):
+    """Generate a random password"""
+    chars = string.ascii_letters + string.digits + string.punctuation
+    for i in range(number):
+        password = "".join(random.choice(chars) for i in range(length))
+        click.echo(password)
 
-num_words = len(words)
-possibilities = (num_words**4) * (10**4)
-print(
-    f"That's amazing! I can't believe this happened! There was a one in {possibilities} chance of that happening!"
-)
+
+if __name__ == "__main__":
+    main()
