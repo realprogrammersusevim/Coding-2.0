9ed001ee2d24a2d04292d7652b573a841b83c39e
realprogrammersusevim <logouts.deco-0e@icloud.com>
2022-09-08 15:58:37 -0500

    make renamer script a command line tool

--- Python/Miscellaneous Scripts/renamer.py
+++ Python/Miscellaneous Scripts/renamer.py
@@ -1,12 +1,20 @@
 import os
+import click
 
-dir = input("Enter the directory path: ")
-fill = len(str(os.listdir(dir))) + 1
-counter = 0
 
-# Rename files to numbers
-for file in os.listdir(dir):
-    counter += 1
-    file_extension = file.split(".")[-1]
-    new_name = f"{str(counter).zfill(fill)}.{file_extension}"
-    os.rename(os.path.join(dir, file), os.path.join(dir, new_name))
+@click.command()
+@click.argument("dir", type=click.Path(exists=True))
+def main(dir):
+    fill = len(str(os.listdir(dir))) + 1
+    counter = 0
+
+    # Rename files to numbers
+    for file in os.listdir(dir):
+        counter += 1
+        file_extension = file.split(".")[-1]
+        new_name = f"{str(counter).zfill(fill)}.{file_extension}"
+        os.rename(os.path.join(dir, file), os.path.join(dir, new_name))
+
+
+if __name__ == "__main__":
+    main()
