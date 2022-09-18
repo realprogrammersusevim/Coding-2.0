da100f4a2a35330807c00f7ab2f3fbf76df9f0ab
Jonathan Milligan <logouts.deco-0e@icloud.com>
2022-09-18 01:32:54 -0500

    Improve rename utility and brute_hw
    The renaming utility finally fulfills it's purpose of renaming every
    file to a number with leading zeroes to make them all the same length. I
    also made the brute_hw Python script a lot easier to read.

--- Python/Miscellaneous_Scripts/brute_hw.py
+++ Python/Miscellaneous_Scripts/brute_hw.py
@@ -1,63 +1,9 @@
 import random
 from time import sleep
+import string
 
 hello_list = ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", "!"]
-letter_list = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    " ",
-    "!",
-]
+letter_list = list(string.ascii_letters + " " + string.punctuation)
 progress = ""
 candidate = ""
 
@@ -65,7 +11,7 @@ for i in range(len(hello_list)):
     while candidate != hello_list[i]:
         candidate = random.choice(letter_list)
         print(f"{progress}{candidate}", end="\r")
-        sleep(0.1)
+        sleep(0.05)
 
     progress += candidate
     candidate = ""
